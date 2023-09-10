6168bfc69605f4d6a2a7440b304754a6adb0e77d
Alper SEVİNÇLER <alper.sevincler@gmail.com>
2023-09-10 08:14:20 +0300

    [AL-3] - Created post method

--- src/main/java/com/archisacademy/openai/controller/WelcomeController.java
+++ src/main/java/com/archisacademy/openai/controller/WelcomeController.java
@@ -1,4 +1,19 @@
 package com.archisacademy.openai.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
 public class WelcomeController {
+
+    @PostMapping("/user")
+    public String gratings(@RequestBody String name) {
+        return null;
+    }
+
+    
+
 }
