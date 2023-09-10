f4bc370955063fe571946c19ef845836a387cb5a
Alper SEVİNÇLER <alper.sevincler@gmail.com>
2023-09-10 08:45:08 +0300

    [AL-5] - Created ChatController in post method

--- src/main/java/com/archisacademy/openai/controller/ChatController.java
+++ src/main/java/com/archisacademy/openai/controller/ChatController.java
@@ -1,7 +1,18 @@
 package com.archisacademy.openai.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("chat")
 public class ChatController {
+
+    @PostMapping("/prompt")
+    public String prompt(@RequestBody String prompt) {
+        // for now
+        return null;
+    }
+
 }
